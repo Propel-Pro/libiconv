2f211b3402a66efd78267d60c36a814b17719524
Lawrence Spector <lawrence.spector@gmail.com>
2025-05-16 14:56:00 -0400

    Run Makefile.devel as single job

--- autogen.sh
+++ autogen.sh
@@ -61,7 +61,7 @@ if test $skip_gnulib = false; then
     $GNULIB_TOOL --copy-file $file || exit $?
     chmod a+x $file || exit $?
   done
-  make -f Makefile.devel \
+  make -j1 -f Makefile.devel \
        gnulib-clean srclib/Makefile.gnulib gnulib-imported-files srclib/Makefile.in \
        GNULIB_TOOL="$GNULIB_TOOL"
 fi
